0df63bdff337d9377d409b9fea58139f4023bd6a
Jose Alberto Galera Sánchez <jagalerasanchez@gmail.com>
2023-07-10 17:27:01 +0200

    chore: add eslint (#1)

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-tab2',
+    selector: 'df-tab2',
     templateUrl: 'tab2.page.html',
     styleUrls: ['tab2.page.scss'],
 })
